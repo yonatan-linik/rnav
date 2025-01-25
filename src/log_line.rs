20ef352d6269f556b06095f71753cb1992af25a5
yonatan-linik <yonatanlinik@gmail.com>
2025-01-25 14:13:01 +0200

    Fixes doc-test for `LogLine`

--- src/log_line.rs
+++ src/log_line.rs
@@ -18,15 +18,33 @@ impl<'a> LogLine<'a> {
     /// # Examples
     /// ```rust
     /// # use rnav::LogLine;
+    /// # use rnav::log_file::LogFile;
     /// # use chrono::offset::FixedOffset;
     /// # use chrono::DateTime;
     /// # use chrono::TimeZone;
+    /// # let file = LogFile::new_with_random_color("test".into(), "test".into());
     /// // RFC3339 format
-    /// assert_eq!(LogLine::new("2021-08-01T12:00:00Z INFO Hello, world!"), Some(LogLine { time: FixedOffset::east_opt(0).unwrap().with_ymd_and_hms(2021, 8, 1, 12, 0, 0).unwrap(), log: "2021-08-01T12:00:00Z INFO Hello, world!", marked: false }));
+    /// assert_eq!(LogLine::new(&file, "2021-08-01T12:00:00Z INFO Hello, world!"),
+    ///            Some(LogLine { src_file: &file,
+    ///                           time: FixedOffset::east_opt(0)
+    ///                             .unwrap()
+    ///                             .with_ymd_and_hms(2021, 8, 1, 12, 0, 0)
+    ///                             .unwrap(),
+    ///                           log: "2021-08-01T12:00:00Z INFO Hello, world!", marked: false
+    ///                         })
+    ///           );
     /// // RFC2822 format
-    /// assert_eq!(LogLine::new("Wed, 18 Feb 2015 23:16:09 GMT Log contents"), Some(LogLine { time: FixedOffset::east_opt(0).unwrap().with_ymd_and_hms(2015, 2, 18, 23, 16, 9).unwrap(), log: "Wed, 18 Feb 2015 23:16:09 GMT Log contents", marked: false }));
+    /// assert_eq!(LogLine::new(&file, "Wed, 18 Feb 2015 23:16:09 GMT Log contents"),
+    ///            Some(LogLine { src_file: &file,
+    ///                           time: FixedOffset::east_opt(0)
+    ///                             .unwrap()
+    ///                             .with_ymd_and_hms(2015, 2, 18, 23, 16, 9)
+    ///                             .unwrap(),
+    ///                           log: "Wed, 18 Feb 2015 23:16:09 GMT Log contents", marked: false
+    ///                         })
+    ///           );
     /// // Unknown format
-    /// assert_eq!(LogLine::new("18/Mar/2003:08:05:30 +0200 Unknown format"), None);
+    /// assert_eq!(LogLine::new(&file, "18/Mar/2003:08:05:30 +0200 Unknown format"), None);
     /// ```
     pub fn new<S: AsRef<str> + ?Sized>(src_file: &'a LogFile, line: &'a S) -> Option<Self> {
         let log = line.as_ref();
